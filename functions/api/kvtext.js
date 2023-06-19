37d6784584f42b699c4b9f20e26545b295ff6cfb
cm-nishida-masayuki <nishida.masayuki@classmethod.jp>
2023-06-19 21:00:25 +0900

    test

--- functions/api/kvtext.js
+++ functions/api/kvtext.js
@@ -1,4 +1,4 @@
 export async function onRequest(context) {
-  const task = await context.env.TODO_LIST.get("test");
+  const task = await context.env.TODO_LIST_2.get("test");
   return new Response(task);
 }
